da4177db785823788164d10cec4de048381eda2e
cczw123 <songju@umich.edu>
2023-02-18 17:04:05 -0500

    update

--- f.m
+++ f.m
@@ -1,3 +1,3 @@
 function out = f(t) %output a1 if it is summer
-out = 4;
+out = 40;
 end
--- solver.m
+++ solver.m
@@ -1,8 +1,10 @@
+set(0,'defaultAxesFontSize',20)
+set(0,'DefaultLineLineWidth', 2)
 % solve odes
 y0 = [20,50]; % initial conditions
 n=1;
 r=0.2;
-K=100;
+K=500;
 a=0;
 d=0.1;
 dw=0.2;
@@ -12,12 +14,13 @@ opts=odeset('relTol',1e-6,'NonNegative',1:numel(y0));
 figure(1);
 plot(T,Y); 
 legend("Speicies","Water")
-
+xlabel("Time")
+ylabel("Amount")
 % solve odes
-y0 = [20,30,50]; % initial conditions
+y0 = [5,15,50]; % initial conditions
 n=2;
 r=[0.2,0.3];
-K=[100,200];
+K=[300,200];
 a=[0,0.1;0,0.1];
 d=[0.1,0.15];
 dw=[0.2,0.2];
@@ -26,7 +29,8 @@ opts=odeset('relTol',1e-6,'NonNegative',1:numel(y0));
 [T,Y] = ode45(@(t,y) rhs(t,y,n,r,K,a,d,dw,Wn),[0,40],y0,opts);
 figure(2);
 plot(T,Y); 
-legend("Speicie1","Specie2","Water")
-
+legend("Speicies 1","Species 2","Water")
+xlabel("Time")
+ylabel("Amount")
 
 
